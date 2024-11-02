1065dd00bbd7ca2857d9b613049299f7992eb187
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2024-11-02 13:07:49 -0500

    Create games.js

---
+++ etc/captiveportal/games.js
@@ -0,0 +1,9 @@
+var games=new Array();
+/*
+This file contains game names and links
+Each game is pushed onto the games array
+
+Example:
+games.push({"caption":"Sudoku","filename":"javascript-sudoku/index.html"});
+
+*/
