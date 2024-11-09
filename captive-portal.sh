0b005a4472537de8497ed261ddaf45a8a429be30
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2024-11-09 09:40:22 -0600

    Update captive-portal.sh

--- captive-portal.sh
+++ captive-portal.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+accesstime=$(date)
+if [[ -n "$NCAT_REMOTE_ADDR" ]]; then
+    # Try using ip neigh first, fallback to arp if it fails
+    MAC_ADDRESS=$(ip neigh show "$NCAT_REMOTE_ADDR" | awk '/..:..:..:..:..:../ {print $5}')
+    if [[ -z "$MAC_ADDRESS" ]]; then
+        MAC_ADDRESS=$(arp -n "$NCAT_REMOTE_ADDR" | awk '/..:..:..:..:..:../ {print $3}')
+    fi
+fi
 if [[ "$1" == "stop" ]]
 then
 killall ncat
@@ -6,10 +14,38 @@ exit 0
 fi
 if [[ "$1" == "http" ]]
 then
+touch /etc/captiveportal/log.count
+source /etc/captiveportal/log.count
+if [[ "$count" == "" ]]
+then
+count=0
+fi
 source /etc/captiveportal/config
-read method query protocol
-accesstime=$(date)
-echo "[$accesstime] $NCAT_REMOTE_ADDR $method $query $protocol" >> /etc/captiveportal/query.log
+read -t 7 method query protocol
+keepreading=1
+count=$(expr $count + 1)
+echo "<br><a href=\"javascript:toggleEntry($count)\">$accesstime</a><br>" >> /etc/captiveportal/log.html_
+echo "<ul class=\"entry\" id=\"entry$count\">" >> /etc/captiveportal/log.html_
+echo "<li>IP Address: $NCAT_REMOTE_ADDR</li>" >> /etc/captiveportal/log.html_
+echo "<li>MAC Address: $MAC_ADDRESS</li>" >> /etc/captiveportal/log.html_
+echo "<li>Method: $method</li>" >> /etc/captiveportal/log.html_
+echo "<li>Query: $query</li>" >> /etc/captiveportal/log.html_
+while [[ "$keepreading" == "1" ]]
+do
+keepreading=0
+metas=
+if read -t 2 meta; then
+keepreading=1
+echo -n "<li>" >> /etc/captiveportal/log.html_
+echo -n "$meta" >> /etc/captiveportal/log.html_
+echo "</li>" >> /etc/captiveportal/log.html_
+metas=$metas $meta
+fi
+done
+echo "</ul>" >> /etc/captiveportal/log.html_
+cat /etc/captiveportal/logheader.html_  /etc/captiveportal/log.html_  /etc/captiveportal/logfooter.html_ > /etc/captiveportal/log.html
+echo "count=$count" > /etc/captiveportal/log.count
+echo "# This file keeps track of the number of requests logged" >> /etc/captiveportal/log.count
 if [[ "$query" == *".."* ]]
 then
 echo "HTTP/1.0 403 Forbidden"
@@ -21,7 +57,40 @@ cat /etc/captiveportal/forbidden.html
 exit 0
 fi
 echo "HTTP/1.0 200 OK"
-arpinfo=$(arp | grep "$NCAT_REMOTE_ADDR")
+if [[ "$query" == "/portallogin/"* ]]
+then
+echo "Content-type: text/html"
+echo ""
+if [[ -f "/etc/captiveportal/users" ]]
+then
+mapfile -t users < /etc/captiveportal/users
+for userline in "${users[@]}"; do
+if [[ "$query" == *"$userline"* ]]
+then
+if [[ "$userline" != "" ]]
+then
+if [[ "$userline" != "#"* ]]
+then
+echo "$query $MAC_ADDRESS" >> /etc/captiveportal/registered/$NCAT_REMOTE_ADDR
+mac-add $MAC_ADDRESS ACCEPT $NCAT_REMOTE_ADDR w
+exit 0
+fi
+fi
+fi
+done
+fi
+echo "Bad username,password or feature disabled";
+exit 0
+fi
+if [[ "$query" == *"/jquery"* ]]
+then
+echo "Content-type: text/javascript"
+contentLength=$(stat -c%s /etc/captiveportal/jquery.js)
+echo "Content-length: $contentLength"
+echo ""
+cat /etc/captiveportal/jquery.js
+exit 0
+fi
 if [[ "$query" == "/favicon.ico" ]]
 then
 echo "Content-type: image/x-icon"
@@ -35,8 +104,11 @@ if [[ "$query" == "/ipinfo.js" ]]
 then
 echo "Content-type: text/javascript"
 echo ""
-echo "document.writeln(\"$arpinfo\");"
-echo "const portalip=\"$portalip\";"
+echo "const MAC_ADDRESS='$MAC_ADDRESS';"
+echo "clientip='$NCAT_REMOTE_ADDR';"
+echo "portalip=\"$portalip\";"
+echo "successurl=\"$redirect\";"
+echo 'document.writeln("MAC Address: "+MAC_ADDRESS+" IP Address: "+clientip);'
 exit 0
 fi
 if [[ "$query" == "/logo.gif" ]]
@@ -48,24 +120,6 @@ echo ""
 cat /etc/captiveportal/logo.gif
 exit 0
 fi
-if [[ "$query" == "/null.gif" ]]
-then
-echo "Content-type: image/gif"
-contentLength=$(stat -c%s /etc/captiveportal/null.gif)
-echo "Content-length: $contentLength"
-echo ""
-cat /etc/captiveportal/null.gif
-exit 0
-fi
-if [[ "$query" == "/log/"* ]]
-then
-echo "Content-type: image/gif"
-contentLength=$(stat -c%s /etc/captiveportal/null.gif)
-echo "Content-length: $contentLength"
-echo ""
-cat /etc/captiveportal/null.gif
-exit 0
-fi
 if [[ "$query" == "/games/"* ]]
 then
 mime=text/html
@@ -73,18 +127,27 @@ if [[ -d "/etc/captiveportal$query" ]]
 then
 echo "Content-type: $mime"
 echo ""
-echo "<!DOCTYPE html><html><body><ul>"
+echo '<!DOCTYPE html><html><body><ul>'
 listing=$(ls "/etc/captiveportal$query" -1)
 IFS=$'\n' read -rd '' -a array <<< "$listing"
 for filename in "${array[@]}"; do
-    echo "<li><a href=\"$filename\">$filename</a></li>"
+    echo -n '<li><a href="'
+    echo -n "$query/$filename"
+    echo -n '">'
+   echo -n "$filename"
+  echo -n '</a></li>'
+echo ""
 done
-echo "</ul></body></html>"
+echo '</ul></body></html>'
 exit 0
 fi
 if [[ "$query" == *".js" ]]
 then
 mime=text/javascript
+fi
+if [[ "$query" == *".xml" ]]
+then
+mime=text/xml
 fi
 if [[ "$query" == *".css" ]]
 then
@@ -103,7 +166,7 @@ then
 mime=image/jpeg
 fi
 echo "Content-type: $mime"
-contentLength=$(stat-c%s /etc/captiveportal$query)
+contentLength=$(stat -c%s /etc/captiveportal$query)
 echo "Content-length: $contentLength"
 echo ""
 cat /etc/captiveportal$query
@@ -113,6 +176,15 @@ echo '<!doctype html><html><body>Could not access file <b>'
 echo "/etc/captiveportal$query"
 echo '</b></body></html>'
 fi
+exit 0
+fi
+if [[ "$query" == "/agree.js" ]]
+then
+echo "Content-type: text/javascript"
+contentLength=$(stat -c%s /etc/captiveportal/agree.js)
+echo "Content-length: $contentLength"
+echo ""
+cat /etc/captiveportal/agree.js
 exit 0
 fi
 if [[ "$query" == "/games.js" ]]
@@ -131,10 +203,23 @@ echo ""
 cat /etc/captiveportal/blockpage.html
 exit 0
 fi
-echo "Starting delphijustin Captive-Portal..."
-if [[ "$1" != "yeshup" ]]
+if [[ "$features" == *"agreement"* ]]
 then
-nohup ncat -k -l 80 --sh-exec "captive-portal.sh http" &
-else
+agreeID=$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM
+echo "?a=$agreeID" > /etc/captiveportal/users
+echo "const agree=\"$agreeID\";" > /etc/captiveportal/agree.js
+fi
+if [[ "$features" == *"renat"* ]]
+then
+natinstall service
+fi
+if [[ "$1" == "httpd" ]]
+then
+echo "Starting delphijustin Captive-Portal(HTTP daemon)..."
 ncat -k -l 80 --sh-exec "captive-portal.sh http"
+fi
+if [[ "$1" == "httpsd" ]]
+then
+echo "Starting delphijustin Captive-Portal(HTTPS daemon)..."
+ncat --ssl --ssl-cert /etc/captiveportal/cert.pem -k -l 8443 --sh-exec "captive-portal.sh http"
 fi
