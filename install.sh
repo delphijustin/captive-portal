c28e108a891ece0a68ef0c7d2ec9c488e1de8545
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2024-11-10 16:02:00 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ then
 cp /etc/captiveportal/config /etc/captiveportal/config.old
 source /etc/captiveportal/config.old
 fi
+apt install -y bind9
 cp -rv etc/ /
 cp bind-db /etc/bind/db.catchall
 if [[ "$portalip" == "" ]]
@@ -20,28 +21,46 @@ echo "* IN A $portalip" >> /etc/bind/db.catchall
 echo "features=\"renat iptables dnsmasq-dhcp\"" >> /etc/captiveportal/config 
 ls /sys/class/net
 read -p "Choose a LAN interface(local network, can be used as wan interface aswell): " lan_interface
+if [[ ! -d /sys/class/net/$lan_interface/ ]]
+then
+echo "LAN_INTERFACE: Doesn't exist"
+exit 1
+else
+if [[ "$lan_interface" == "" ]]
+then
+exit 1
+fi
+fi
 read -p "Choose a WAN interface(Internet network): " wan_interface
-echo "lan_interface=$lan_interface" >> /etc/captiveportal/config
+if [[ ! -d /sys/class/net/$wan_interface/ ]]
+then
+echo "WAN_INTERFACE: Doesn't exist"
+exit 1
+else
+if [[ "$lan_interface" == "" ]]
+then
+exit 1
+fi
+fi
+echo "lan_interface=$lan_
+interface" >> /etc/captiveportal/config
 echo "wan_interface=$wan_interface" >> /etc/captiveportal/config
 # Split the IP address into an array using dot as the delimiter
 IFS='.' read -r -a ip_array <<< "$portalip"
-if [[ "$ip_array[0]" == "10" ]]
+if [[ "${ip_array[0]}" == "10" ]]
 then
 IPConfigured=1
 echo "localnet=10.0.0.0/8" >> /etc/captiveportal/config
 fi
-if [[ "$ip_array[0].$ip_array[1]" == "192.168" ]]
+if [[ "${ip_array[0]}.${ip_array[1]}" == "192.168" ]]
 then
-echo "localnet=192.168.$ip_array[2].0/24" >> /etc/captiveportal/config
+echo "localnet=192.168.${ip_array[2]}.0/24" >> /etc/captiveportal/config
 IPConfigured=3
 fi
-if [[ "$ip_array[0]" == "172" ]]
+if [[ "${ip_array[0]}" == "172" && ${ip_array[1]} -lt 32 && ${ip_array[1]} -gt 15 ]]
 then
-if [[ $ip_array[1] -gt 15 && $ip_array[1] -lt 32 ]]
-then
-echo "localnet=172.$ip_array[1].0.0/16" >> /etc/captiveportal/config
+echo "localnet=172.${ip_array[1]}.0.0/16" >> /etc/captiveportal/config
 IPConfigured=2
-fi
 fi
 if [[ "$IPConfigured" == "0" ]]
 then
@@ -49,28 +68,29 @@ echo "Could not detect your local IP class."
 echo "Please make sure you enter the local ip address for your portal IP not your internet ip.."
 exit 1
 fi
-apt install ncat bind9 dnsmasq
+apt install -y ncat dnsmasq
 cp /etc/dnsmasq.conf /etc/captiveportal/dnsmasq.conf.off
 echo "port=0" > /etc/dnsmasq.conf
 echo "dhcp-option=3,$portalip" >> /etc/dnsmasq.conf
 echo "dhcp-option=6,$portalip" >> /etc/dnsmasq.conf
 case $IPConfigured in
  1)
- dhcpRange=$ip_array[0].0.0.1,$ip_array[0].255.255.254
+ dhcpRange=${ip_array[0]}.0.0.1,${ip_array[0]}.255.255.254
  ;;
  2)
- dhcpRange=$ip_array[0].$ip_array[1].0.1,$ip_array[0].$ip_array[1].255.254
+ dhcpRange=${ip_array[0]}.${ip_array[1]}.0.1,${ip_array[0]}.${ip_array[1]}.255.254
  ;;
  3)
- dhcpRange=$ip_array[0].$ip_array[1].$ip_array[2].1,$ip_array[0].$ip_array[1].$ip_array[2].254
+ dhcpRange=${ip_array[0]}.${ip_array[1]}.${ip_array[2]}.1,${ip_array[0]}.${ip_array[1]}.${ip_array[2]}.254
  ;;
 esac
+cat /etc/captiveportal/named.conf.header /etc/captiveportal/named.conf.footer1 /etc/captiveportal/named.conf.footer2 > /etc/bind/named.conf.local
 echo "dhcp-range=$dhcpRange,7d" >> /etc/dnsmasq.conf
-chmod +x /usr/locall/bin/gameserver
-chmod +x /usr/locall/bin/mac-add
-chmod +x /usr/locall/bin/captive-portal.sh
-chmod +x /usr/locall/bin/natinstall
-chmod +x /usr/locall/bin/cpuser
+chmod +x /usr/local/bin/gameserver
+chmod +x /usr/local/bin/mac-add
+chmod +x /usr/local/bin/captive-portal.sh
+chmod +x /usr/local/bin/natinstall
+chmod +x /usr/local/bin/cpuser
 natinstall
 gameserver
 cp captive-portal.service /etc/systemd/system/
