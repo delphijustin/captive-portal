614d4cc2fdeaa4736335df478a3c526fec94c7aa
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2024-11-15 20:03:35 -0600

    Create captiveportal.js

---
+++ etc/captiveportal/captiveportal.js
@@ -0,0 +1,30 @@
+const params = new URLSearchParams(window.location.search);
+function retryInternet(){
+var trys=Number(params.get("portalsuccess"))+1;
+location.href="/portalsuccess/?portalsuccess="+trys;
+}
+function blockedPageStart(){
+if(games.length==0){
+document.getElementById("nogames").style.display="block";
+}
+games.forEach(function(game){
+document.getElementById("games").innerHTML+='<li><a href="//'+portalip+'/games/'+game.filename+'">'+game.caption+'</a></li>';
+});
+}
+function login(form){
+document.getElementById("error").innerHTML="Please wait...";
+$.get("/portallogin/"+form.u.value.toLowerCase()+"/"+form.p.value,
+function(results){
+document.getElementById("error").innerHTML=results;
+if(results.indexOf("Welcome")>0)setTimeout(function(){
+location.href="/portalsuccess/?portalsuccess=1";
+},10000);
+});
+}
+function browseTheWeb(){
+location.href=successurl;
+}
+function agree(o){
+if(o.value!="I Agree")return;
+location.href="/portallogin/agree/"+agreeID;
+}
